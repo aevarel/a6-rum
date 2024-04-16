8fccc0ffc8f93f16efc43789e57cf32ca888f552
aevarel <aevaternum@gmail.com>
2024-04-16 16:29:49 -0400

    skeleton code for unpacking the opcode, code for unpacking the registers of a given instruction word

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,40 @@
-//use::bitpack::*;
+use::bitpack::*;
 pub mod math;
 pub mod io;
 pub mod control;
 pub mod memory;
+
+/// Extracts the opcode from an instruction word
+/// 
+/// # Arguments
+/// 
+/// * `word` - the instruction word
+/// 
+/// # Returns
+/// 
+/// The opcode
+/// 
+/// # Panics
+/// 
+/// If the opcode is not a valid value
+#[inline]
+//pub fn opcode(word: u32) -> u8 {  } 
+
+
+/// Extracts the registers from an instruction word
+/// 
+/// # Arguments
+/// 
+/// * `word` - the instruction word
+/// 
+/// # Returns
+/// 
+/// An array of 3 u8s representing the registers
+#[inline]
+pub fn regs_array(word: u32) -> [u8; 3] {
+    let a = getu(word as u64, 3, 0).unwrap() as u8;
+    let b = getu(word as u64, 3, 3).unwrap() as u8;
+    let c = getu(word as u64, 3, 6).unwrap() as u8;
+
+    [a, b, c]
+}
--- src/math.rs
+++ src/math.rs
@@ -1,12 +1,14 @@
 use::bitpack::*;
 
-
+// will probably need a test module to make sure that the referenced register is valid 
 /* 
     Add
     Arguments: reference to registers array, current instruction word
     Returns: 0 on success, 31-39 on failure
 */
 pub fn add(r: &mut [u32; 8], iw: u32) -> u32 {
+        // $r[A] := ($r[B] + $r[C]) mod 23^2
+        
     return 0;
 }
 
@@ -17,6 +19,7 @@ pub fn add(r: &mut [u32; 8], iw: u32) -> u32 {
     Returns: 0 on success, 41-49 on failure
 */
 pub fn mul(r: &mut [u32; 8], iw: u32) -> u32 {
+        // $r[A] := ($r[B] × $r[C]) mod 232
     return 0;
 }
 
@@ -26,6 +29,7 @@ pub fn mul(r: &mut [u32; 8], iw: u32) -> u32 {
     Returns: 0 on success, 51-59 on failure
 */
 pub fn div(r: &mut [u32; 8], iw: u32) -> u32 {
+        // $r[A] := ($r[B] ÷ $r[C]) (integer division)
     return 0;
 }
 
@@ -35,5 +39,6 @@ pub fn div(r: &mut [u32; 8], iw: u32) -> u32 {
     Returns: 0 on success, 61-69 on failure
 */
 pub fn nand(r: &mut [u32; 8], iw: u32) -> u32 {
+        // $r[A] :=¬($r[B]∧$r[C])
     return 0;
 }
